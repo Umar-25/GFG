49268557d75dd9ae3feb855118d88ed35c0479a4
Umar-25 <132724909+Umar-25@users.noreply.github.com>
2025-01-30 23:13:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.java
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.java
@@ -17,6 +17,16 @@ class GFG {
             if (ans.size() == 0)
                 System.out.println("-1");
             else {
+                ans.sort((list1, list2) -> {
+                    int size = Math.min(list1.size(), list2.size());
+                    for (int i = 0; i < size; i++) {
+                        if (!list1.get(i).equals(list2.get(i))) {
+                            return list1.get(i) - list2.get(i);
+                        }
+                    }
+                    return list1.size() - list2.size();
+                });
+
                 for (int i = 0; i < ans.size(); i++) {
                     System.out.print("[");
                     for (int j = 0; j < ans.get(i).size(); j++)
@@ -25,6 +35,8 @@ class GFG {
                 }
                 System.out.println();
             }
+
+            System.out.println("~");
         }
     }
 }
@@ -34,43 +46,25 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    private ArrayList<ArrayList<Integer>> result;
-    private int[] row;
-
-    public Solution() {
-        result = new ArrayList<>();
-        row = new int[10];
+    public ArrayList<ArrayList<Integer>> nQueen(int n) {
+        if (n < 4 && n != 1) return new ArrayList<>();
+        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        int[] row = new int[n];
+        solve(0, 0, 0, 0, n, row, res);
+        return res;
     }
 
-    private boolean place(int r, int c) {
-        for (int prev = 0; prev < c; prev++) {
-            if (row[prev] == r || Math.abs(row[prev] - r) == Math.abs(prev - c)) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    private void bt(int c, int n) {
+    private void solve(int c, int cols, int d1, int d2, int n, int[] row, ArrayList<ArrayList<Integer>> res) {
         if (c == n) {
-            ArrayList<Integer> v = new ArrayList<>();
-            for (int i = 0; i < n; i++) {
-                v.add(row[i] + 1);
-            }
-            result.add(v);
+            ArrayList<Integer> temp = new ArrayList<>();
+            for (int r : row) temp.add(r + 1);
+            res.add(temp);
             return;
         }
-        for (int i = 0; i < n; i++) {
-            if (place(i, c)) {
-                row[c] = i;
-                bt(c + 1, n);
+        for (int r = 0, pos = 1; r < n; ++r, pos <<= 1) 
+            if ((cols & pos) == 0 && (d1 & (pos << c)) == 0 && (d2 & (pos << (n - 1 - c))) == 0) {
+                row[c] = r;
+                solve(c + 1, cols | pos, d1 | (pos << c), d2 | (pos << (n - 1 - c)), n, row, res);
             }
-        }
-    }
-
-    public ArrayList<ArrayList<Integer>> nQueen(int n) {
-        result.clear();
-        bt(0, n);
-        return result;
     }
 }
