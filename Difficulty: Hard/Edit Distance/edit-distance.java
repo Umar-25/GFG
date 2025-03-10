ee6a4c8bde153567dd58f792eea76d00e0069bf1
Umar-25 <132724909+Umar-25@users.noreply.github.com>
2025-03-10 23:42:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Edit Distance/edit-distance.java
+++ Difficulty: Hard/Edit Distance/edit-distance.java
@@ -9,12 +9,13 @@ class GFG {
         int T = Integer.parseInt(br.readLine().trim());
         while (T-- > 0) {
             String s1 = br.readLine();
-            String[] S = s1.split(" ");
-            String s = S[0];
-            String t = S[1];
+            String s2 = br.readLine();
+
             Solution ob = new Solution();
-            int ans = ob.editDistance(s, t);
+            int ans = ob.editDistance(s1, s2);
             System.out.println(ans);
+
+            System.out.println("~");
         }
     }
 }
@@ -22,38 +23,19 @@ class GFG {
 // } Driver Code Ends
 
 
-
-
 class Solution {
-    public int ans(String str1, String str2, int n, int m, int[][] dp) {
-        if (n == -1) return m + 1;
-        if (m == -1) return n + 1;
-        if (dp[n][m] != Integer.MAX_VALUE) return dp[n][m];
-        
-        if (str1.charAt(n) == str2.charAt(m)) {
-            return dp[n][m] = Math.min(dp[n][m], ans(str1, str2, n - 1, m - 1, dp));
+    public int editDistance(String s1, String s2) {
+        int m = s1.length(), n = s2.length();
+        int[] prev = new int[n + 1], curr = new int[n + 1];
+        for (int i = 0; i <= n; i++) prev[i] = i;
+        for (int i = 1; i <= m; i++) {
+            curr[0] = i;
+            for (int j = 1; j <= n; j++)
+                curr[j] = s1.charAt(i-1) == s2.charAt(j-1) ? prev[j-1] : 1 + Math.min(prev[j-1], Math.min(prev[j], curr[j-1]));
+            int[] temp = prev;
+            prev = curr;
+            curr = temp;
         }
-        
-        dp[n][m] = Math.min(dp[n][m], 1 + ans(str1, str2, n - 1, m - 1, dp));
-        dp[n][m] = Math.min(dp[n][m], 1 + ans(str1, str2, n - 1, m, dp));
-        dp[n][m] = Math.min(dp[n][m], 1 + ans(str1, str2, n, m - 1, dp));
-        
-        return dp[n][m];
-    }
-    
-    public int editDistance(String str1, String str2) {
-        int n = str1.length(), m = str2.length();
-        int[][] dp = new int[n][m];
-        for (int[] row : dp) {
-            Arrays.fill(row, Integer.MAX_VALUE);
-        }
-        return ans(str1, str2, n - 1, m - 1, dp);
-    }
-
-    public static void main(String[] args) {
-        Solution sol = new Solution();
-        String str1 = "intention";
-        String str2 = "execution";
-        System.out.println(sol.editDistance(str1, str2)); // Output: 5
+        return prev[n];
     }
 }
